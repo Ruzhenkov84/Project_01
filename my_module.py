f7a85103fc389208085fd3adf716269898331298
Vladimir Rizhenkov <vladimir.rizhenckov@yandex.ru>
2022-11-09 22:38:06 +0300

    Dz#7

--- my_module.py
+++ my_module.py
@@ -1,29 +1,16 @@
-# Задача 6
-# Есть словарь песен 
-# Распечатайте общее время звучания трех случайных песен
-# Вывод: Три песни звучат ХХХ минут.
-from audioop import add
-import random
+from random import randint
 
-my_favorite_songs = {
-    'Waste a Moment': 3.03,
-    'New Salvation': 4.02,
-    'Staying\' Alive': 3.40,
-    'Out of Touch': 3.03,
-    'A Sorta Fairytale': 5.28,
-    'Easy': 4.15,
-    'Beautiful Day': 4.04,
-    'Nowhere to Run': 2.58,
-    'In This World': 4.02,
-    }
-song=[]
-for i in range(3):
-    song.append(random.choice(list(my_favorite_songs.items())))
-k = 0
-for name,time in song:
-    k+=time
-print(song)   
-print('Три песни звучат',(k), 'минут')
+n = 5
+arr =[] 
+for i in range(n):
+    arr.append(randint(1,1000))
+for i in range(n-1):
+    for k in range(n-1):
+        if arr[k]>arr[k+1]:
+            arr[k],arr[k+1]=arr[k+1],arr[k]
+print("min = ",arr[1]) 
+
+print("max = ",arr [n-1])
 
 
 
